080b59a391890a6141e68cea8e86edbee94de0f9
Ihab P <113042103+ITP2023@users.noreply.github.com>
2023-07-01 13:32:04 +0300

    commited necessary changes

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -15,10 +15,9 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Contents',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: "Who is the Prisoner?", link: "/whoami" }
         ]
       }
     ],
